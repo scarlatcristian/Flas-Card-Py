096e0fdbcbf1c0b0b2adb03561bc73742514306d
cristi <scarlat.cristian.gabriel@gmail.com>
2023-11-09 23:36:05 +0200

    added 3 sec timer to flip card

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ current_card = {}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card['French'])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card['French'], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
     global current_card
-    canvas.itemconfig(card_title, text='English')
-    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_title, text='English', fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
 
@@ -27,7 +30,7 @@ window = Tk()
 window.title("Flash card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 # Canvas
 canvas = Canvas(width=800, height=526)
